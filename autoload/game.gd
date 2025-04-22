2a512b70f8b04aa6ffbfa7341056d2241a1e7f28
Harmoen <harmon.the.emu@gmail.com>
2025-04-22 14:20:12 -0500

    Changed Project Icon

--- autoload/game.gd
+++ autoload/game.gd
@@ -4,7 +4,10 @@ const TOTAL_TUTORIALS := 6
 const SAVE_PATH := "user://save_data.json"
 const PROGRESS_PATH := "user://progress.json"
 
-var current_level_index := 0
+signal game_paused
+signal game_unpaused
+
+var current_level_index : int = 0
 var is_in_tutorial := false
 var tutorials_completed := false
 
@@ -15,6 +18,7 @@ var high_score := 0
 var transition_level := 0
 
 func _ready():
+	set_deferred("process_mode", ProcessMode.PROCESS_MODE_ALWAYS)
 	load_data()
 	load_progress()
 	start_next_level()
@@ -66,11 +70,12 @@ func load_progress():
 		tutorials_completed = progress_data.get("tutorials_completed", false)
 
 func save_data():
+	@warning_ignore("shadowed_variable")
 	var save_data = {
 		"player_name": player_name,
 		"high_score": high_score
 	}
-
+	
 	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
 	file.store_line(JSON.stringify(save_data))
 	file.close()
@@ -87,3 +92,19 @@ func load_data():
 	if data:
 		player_name = data.get("player_name", "")
 		high_score = data.get("high_score", 0)
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		if get_tree().paused:
+			game_unpaused.emit()
+		else:
+			game_paused.emit()
+
+
+func _on_game_paused() -> void:
+	get_tree().paused = true
+
+
+func _on_game_unpaused() -> void:
+	get_tree().paused = false
