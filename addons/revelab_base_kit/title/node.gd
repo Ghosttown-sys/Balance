dfc39be0b874947e024d849844429f94bc17e057
Harmoen <harmon.the.emu@gmail.com>
2025-04-26 03:05:34 -0500

    Changed icon tweens to shader

--- addons/revelab_base_kit/title/node.gd
+++ addons/revelab_base_kit/title/node.gd
@@ -1,59 +1,24 @@
 extends Node
 
-@onready var moon: Sprite2D = $Moon
-@onready var rain: Sprite2D = $Rain
-@onready var snow: Sprite2D = $Snow
-@onready var sun: Sprite2D = $Sun
-@onready var thunder: Sprite2D = $Thunder
-@onready var wind: Sprite2D = $Wind
+@export var SYMBOL_COLORS : Dictionary[Node, Color]= {}
 
-var SYMBOL_COLORS := {
-	"moon": Color("#a4d6f8"),
-	"sun": Color("#ffd965"),
-	"rain": Color("#00bfff"),
-	"snow": Color("#00f0ff"),
-	"thunder": Color("#bf40bf"),
-	"wind": Color("#7fffd4")
-}
-
-var tween: Tween
 
 func _ready():
 	_set_initial_colors()
-	_start_color_pulse_animation()
+
 
 func _set_initial_colors():
-	moon.modulate = SYMBOL_COLORS["moon"]
-	rain.modulate = SYMBOL_COLORS["rain"]
-	snow.modulate = SYMBOL_COLORS["snow"]
-	sun.modulate = SYMBOL_COLORS["sun"]
-	thunder.modulate = SYMBOL_COLORS["thunder"]
-	wind.modulate = SYMBOL_COLORS["wind"]
-
-func _start_color_pulse_animation():
-	if tween:
-		tween.kill()
-	
-	tween = create_tween()
-	tween.set_loops()
-	
-	tween.set_parallel(true)
-	
-	_pulse_symbol(moon, "moon", randf_range(0.1, 0.5))
-	_pulse_symbol(sun, "sun", randf_range(0.1, 0.5))
-	_pulse_symbol(rain, "rain", randf_range(0.1, 0.5))
-	_pulse_symbol(snow, "snow", randf_range(0.1, 0.5))
-	_pulse_symbol(thunder, "thunder", randf_range(0.1, 0.5))
-	_pulse_symbol(wind, "wind", randf_range(0.1, 0.5))
-
-func _pulse_symbol(sprite: Sprite2D, color_key: String, initial_delay: float):
-	var duration = randf_range(1.5, 2.5)
-	var delay = randf_range(1.0, 2.0)
-	
-	tween.tween_property(sprite, "modulate", Color.WHITE, duration)\
-		.set_ease(Tween.EASE_IN_OUT)\
-		.set_delay(initial_delay)
-	
-	tween.tween_property(sprite, "modulate", SYMBOL_COLORS[color_key], duration)\
-		.set_ease(Tween.EASE_IN_OUT)\
-		.set_delay(initial_delay + duration + delay)
+	var i : int = 0
+	for node : Node in SYMBOL_COLORS:
+		var shader : ShaderMaterial = node.get("material") as ShaderMaterial
+		#region safety checks
+		if not shader:
+			return
+		if not shader.get("shader"):
+			return
+		#endregion safety checks
+		
+		shader.set_shader_parameter("end_color",SYMBOL_COLORS.get(node))
+		shader.set_shader_parameter("start_offset", i * 0.6)
+		shader.set_shader_parameter("speed", 1.2)
+		i += 1
